9940f7be16f2680482eefe124d875798ccb6e0f3
pesala-x <gkpesalawinodith@gmail.com>
2024-08-29 13:31:19 +0530

    dashboard response handled..

--- Frontend-JWT/js/dashboard.js
+++ Frontend-JWT/js/dashboard.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
   let token = localStorage.getItem('token');
-  if (!token) {
+  if (token) {
+    $.ajax({
+      url: 'http://localhost:8080/api/v1/blog/newMethod',
+      method: 'GET',
+      contentType: 'application/json',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+
+      success: function (response) {
+        console.log(response);
+        $("#hello").append(response)
+          alert("The response is : " + response);
+      },
+
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  }else {
     window.location.href = 'Sign-in.html';
   }
 });
@@ -8,7 +27,6 @@ $(document).ready(function() {
 function logOut() {
   // Remove the token from local storage
   localStorage.removeItem('token')
-
   // Redirect to the login page after logout
   window.location.href = "Sign-in.html";
 }
